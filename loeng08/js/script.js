7f725cd8ec40d828946353205253342d4e359a29
kuttim <kuttim.mikk@gmail.com>
2022-04-13 14:35:04 +0300

    distinguish short and long name images

--- loeng08/js/script.js
+++ loeng08/js/script.js
@@ -7,10 +7,10 @@ if (firstName.length >= 6) {
     '!' +
     ' Küll sul on pikk suurepärane nimi.';
   document.getElementById('image').innerHTML =
-    "<img src='https://picsum.photos/200' />";
+    "<img src='https://picsum.photos/300' />";
 } else {
   document.getElementById('name').innerHTML =
     'Tere, ' + firstName + '!' + ' Küll sul on imeline lühike nimi.';
   document.getElementById('image').innerHTML =
-    "<img src='https://picsum.photos/300' />";
+    "<img src='https://picsum.photos/200' />";
 }
