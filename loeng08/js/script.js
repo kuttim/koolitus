99e9e355e8715e06f321ce1e144ca12955d1ad3b
kuttim <kuttim.mikk@gmail.com>
2022-04-13 13:18:28 +0300

    homework

--- loeng08/js/script.js
+++ loeng08/js/script.js
@@ -3,7 +3,11 @@ let firstName = prompt('Mis on sinu nimi?');
 if (firstName.length >= 6) {
   document.getElementById('name').innerHTML =
     'Tere ' + firstName + ' Küll sul on pikk suurepärane nimi.';
+  document.getElementById('image').innerHTML =
+    "<img src='https://picsum.photos/200' />";
 } else {
   document.getElementById('name').innerHTML =
     'Tere ' + firstName + ' Küll sul on imeline lühike nimi.';
+  document.getElementById('image').innerHTML =
+    "<img src='https://picsum.photos/200' />";
 }
