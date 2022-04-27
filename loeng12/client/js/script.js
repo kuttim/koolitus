2392fe146e3b8cd4ae3ac0192b728e44697ddb9a
kuttim <kuttim.mikk@gmail.com>
2022-04-27 21:32:18 +0300

    fix multiplication bug

--- loeng12/client/js/script.js
+++ loeng12/client/js/script.js
@@ -2,7 +2,6 @@ const inputField = document.getElementById('input');
 
 let isOperatorLastClicked = false;
 let numbers = [];
-
 const clickedNumber = (number) => {
   if (inputField.innerHTML === '0' || isOperatorLastClicked) {
     inputField.innerHTML = number;
@@ -24,17 +23,17 @@ const clickedOperator = (operator) => {
 const clickedEquals = async () => {
   numbers.push(inputField.innerHTML);
   console.log('equals!!', numbers);
-  const response = await fetch(
-    'https://88ec-85-196-192-119.eu.ngrok.io/api/add',
-    {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(numbers),
-    }
-  );
+  const savedNumbers = [].concat(numbers);
+  numbers.splice(0, numbers.length);
+
+  const response = await fetch('http://localhost:3000/api/add', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(savedNumbers),
+  });
   const answer = await response.json();
   console.log('vastus:', answer);
   input.innerHTML = [answer.answer];
